6040892fef084858ce4a0eae5a2dd0f6416c07d3
Khaled-AL-ALi <khaled2020pro@gmail.com>
2021-12-16 16:32:50 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
+//import Search from "./components/Search";
 
-import SayHi, { SayHello } from "./components/WeatherItem";
+//import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
-
+import clear from "./img/weather-icons/clear.svg";
+import cloudy from "./img/weather-icons/partlycloudy.svg";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,9 +22,63 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
+       {/* <SayHi />
         <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+       <Search handleInput={this.handleInputChange} />*/}
+
+     <div className="NAV">
+            <input type="text"/>
+            <input type="submit" value="Find Weather"/>
+     </div>
+     <div className="BigDesc">
+            <img src={cloudy} />
+            <p>Overcast clouds</p>
+            <p><span>temprature</span> 10° to 11°C</p>
+            <p><span>Humidity</span> 78% <span>pressure</span> 1008.48</p>
+     </div>
+     <div className="DailyDesc">
+              <div>
+                    <p>03:00 </p>
+                    <img src={cloudy} />
+                    <p>8°C</p>
+              </div>
+              <div>
+                    <p>06:00 </p>
+                    <img src={cloudy}  />
+                    <p>9°C</p>
+              </div>
+              <div>
+                    <p>09:00 </p>
+                    <img src={clear}  />
+                    <p>14°C</p>
+              </div>
+              <div>
+                    <p>12:00 </p>
+                    <img src={clear}  />
+                    <p>17°C</p>
+              </div>
+              <div>
+                    <p>15:00 </p>
+                    <img src={clear} />
+                    <p>18°C</p>
+              </div>
+              <div>
+                    <p>18:00 </p>
+                    <img src={clear}  />
+                    <p>16°C</p>
+              </div>
+              <div>
+                    <p>21:00 </p>
+                    <img src={cloudy}  />
+                    <p>13°C</p>
+              </div>
+
+
+     </div>
+
+
+
+
       </div>
     );
   }
